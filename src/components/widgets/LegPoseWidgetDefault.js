4137eb9a61cb0031ceaf6408db9a60eea91a9ce3
Mithi <mithi.sevilla@gmail.com>
2020-05-21 20:00:29 +0800

    Move leg pose widget to its own module

---
+++ src/components/widgets/LegPoseWidgetDefault.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+
+const LegPoseInputField = (props) => {
+    return (
+      <div className="cell">
+        <label htmlFor={props.name} className="label">
+          {props.name}
+        </label>
+        <input
+          type="number"
+          id={props.name}
+          value={props.value}
+          className="input"
+          onChange={(e) => props.handleChange(props.name, e.target.value)}
+          min={-180}
+          max={180}
+          step={0.5}
+        />
+      </div>
+    )
+  }
+
+class LegPoseWidget extends Component {
+  updateFieldState = (angle, value) => {
+    value = Math.min(Math.max(value, -180), 180)
+    this.props.onUpdate(this.props.name, angle, value)
+  }
+
+   render() {
+    const inputFields = Object.keys(this.props.pose).map((name) => {
+      return (
+        <LegPoseInputField
+          key={name}
+          name={name}
+          value={this.props.pose[name]}
+          handleChange={this.updateFieldState}
+        />
+      )
+    })
+
+     return (
+      <div className="column-container">
+        <h3>{this.props.name}</h3>
+        <form className="row-container">{inputFields}</form>
+      </div>
+    )
+  }
+}
+
+export default LegPoseWidget
