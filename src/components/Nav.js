ebf145197d4166b2e73e1df8bd9ad950702b0fde
Mithi <mithi.sevilla@gmail.com>
2020-05-22 21:57:43 +0800

    Refactor link widgets

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,82 +1,67 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { BasicLink, PageLink } from "./widgets/generic/LinkWidgets"
 
 const URL_KOFI = "https://ko-fi.com/minimithi"
 const URL_SRC = "https://github.com/mithi/hexapod-robot-simulator"
+
 const ICON_KOFI = "🍵"
-const ICON_DOT = "●"
 const ICON_SRC = "👾"
+
 const PATH_IK = "inverse-kinematics"
 const PATH_FK = "forward-kinematics"
 const PATH_LP = "leg-patterns"
-
 const PATH_ROOT = "/"
+
+const KOFI_LINK_TXT = "🍵 Buy Mithi coffee"
+const SRC_LINK_TXT = "🍵 Source code"
 const ROOT_LINK_TXT = "💋 Root"
-const IK_LINK_TXT = `${ICON_DOT} Inverse Kinematics`
-const FK_LINK_TXT = `${ICON_DOT} Forward Kinematics`
-const PATTERNS_LINK_TXT = `${ICON_DOT} Patterns`
-const KOFI_LINK_TXT = `${ICON_KOFI} Buy Mithi coffee (or tea)`
-const SRC_LINK_TXT = `${ICON_SRC} Source code`
 
-const BasicLink = props => {
-    return (
-        <a
-            className={`link-icon ${props.klass}`}
-            href={props.path}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <span>{props.symbol}</span>
-        </a>
-    )
-}
+const IK_LINK_TXT = "● Inverse Kinematics"
+const FK_LINK_TXT = "● Forward Kinematics"
+const LP_LINK_TXT = "● Patterns"
 
-const PageLink = props => {
-    return (
-        <div className={`link-icon ${props.klass || ""}`}>
-            <Link to={props.path}>{props.symbol}</Link>
-        </div>
-    )
-}
+const URLS = [URL_KOFI, URL_SRC]
+const URL_LINK_TXTS = [KOFI_LINK_TXT, SRC_LINK_TXT]
+const PATHS = [PATH_ROOT, PATH_FK, PATH_IK, PATH_LP]
+const LINK_TXTS = [ROOT_LINK_TXT, FK_LINK_TXT, IK_LINK_TXT, LP_LINK_TXT]
+
+const PAGE_LINKS = PATHS.map((path, index) => (
+    <PageLink
+        path={path}
+        symbol={LINK_TXTS[index]}
+        klass="nav-footer-link"
+    />
+))
+
+const BASIC_LINKS = URLS.map((path, index) => (
+    <BasicLink
+        path={path}
+        symbol={URL_LINK_TXTS[index]}
+        klass="nav-footer-link"
+    />
+))
+
+
+
 
 const NavBar = () => (
     <div className="row-container">
         <BasicLink path={URL_KOFI} symbol={ICON_KOFI} />
         <BasicLink path={URL_SRC} symbol={ICON_SRC} />
-        <PageLink path={PATH_ROOT} symbol={ICON_DOT} />
-        <PageLink path={PATH_FK} symbol={ICON_DOT} />
-        <PageLink path={PATH_IK} symbol={ICON_DOT} />
-        <PageLink path={PATH_LP} symbol={ICON_DOT} />
+        {PATHS.map(path => {
+            return <PageLink path={path} symbol="●" />
+         })}
     </div>
 )
 
 const NavFooter = () => (
     <div className="row-container nav-footer">
         <div className="column-container cell">
-            <BasicLink
-                path={URL_KOFI}
-                symbol={KOFI_LINK_TXT}
-                klass="nav-footer-link"
-            />
-            <BasicLink
-                path={URL_SRC}
-                symbol={SRC_LINK_TXT}
-                klass="nav-footer-link"
-            />
-            <PageLink
-                path={PATH_ROOT}
-                symbol={ROOT_LINK_TXT}
-                klass="nav-footer-link"
-            />
+            {BASIC_LINKS}
+            {PAGE_LINKS[0]}
         </div>
         <div className="column-container cell">
-            <PageLink path={PATH_FK} symbol={FK_LINK_TXT} klass="nav-footer-link" />
-            <PageLink path={PATH_IK} symbol={IK_LINK_TXT} klass="nav-footer-link" />
-            <PageLink
-                path={PATH_LP}
-                symbol={PATTERNS_LINK_TXT}
-                klass="nav-footer-link"
-            />
+            {PAGE_LINKS.slice(1)}
         </div>
     </div>
 )
