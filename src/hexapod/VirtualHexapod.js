ba3ffe2253c3fceb93af28c2b5c090ad05dfc731
Mithi <mithi.sevilla@gmail.com>
2020-05-26 00:45:52 +0800

    Fix all the current bugs in Virtual Hexapod

--- src/hexapod/VirtualHexapod.js
+++ src/hexapod/VirtualHexapod.js
@@ -1,8 +1,9 @@
 import { createVector, createHexagon } from "./basicObjects"
 import Linkage from "../hexapod/Linkage"
 import { POSITION_LIST } from "./constants"
+import { POSE } from "../templates/hexapodParams"
 
-const neutralLocalFrame = {
+const DEFAULT_LOCAL_FRAME = {
     xAxis: createVector(1, 0, 0, "hexapodXaxis"),
     yAxis: createVector(0, 1, 0, "hexapodYaxis"),
     zAxis: createVector(0, 0, 1, "hexapodZaxis"),
@@ -12,26 +13,40 @@ class VirtualHexapod {
     constructor(
         bodyDimensions = { front: 100, middle: 100, side: 100 },
         legDimensions = { coxia: 100, femur: 100, tibia: 100 },
-        pose
+        pose = POSE
     ) {
-        const localNeutralFrame = neutralLocalFrame()
-        // create preliminary hexagon
-        const neutralHexagon = createHexagon(
-            bodyDimensions.front,
-            bodyDimensions.middle,
-            bodyDimensions.side
-        )
+        const neutralLocalFrame = DEFAULT_LOCAL_FRAME
+        const neutralHexagon = createHexagon(bodyDimensions)
 
-        // preliminary legs
         const legListWithoutGravity = POSITION_LIST.map(
             (position, index) =>
                 new Linkage(
                     legDimensions,
                     position,
-                    this.neutralHexagon.verticesList[index],
+                    neutralHexagon.verticesList[index],
                     pose[position]
                 )
         )
+
+        this.legs = legListWithoutGravity
+        this.body = neutralHexagon
+        this.localFrame = neutralLocalFrame
+        this.cogProjection = {
+            x: this.body.cog.x,
+            y: this.body.cog.y,
+            z: 0,
+            name: "centerOfGravityProjectionPoint",
+        }
+
+        this.groundContactPoints = []
+
+        this.sumOfDimensions =
+            bodyDimensions.front +
+            bodyDimensions.middle +
+            bodyDimensions.side +
+            legDimensions.coxia +
+            legDimensions.femur +
+            legDimensions.tibia
 
         // STEP 1:
         // Find new orientation of the body (new normal)
@@ -51,3 +66,5 @@ class VirtualHexapod {
     // getTranslatedHexapod
     // getStancedHexapod
 }
+
+export default VirtualHexapod
