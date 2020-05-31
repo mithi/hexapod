9bc0980730ff65de661493300e6176f2abc531c1
Mithi <mithi.sevilla@gmail.com>
2020-05-31 19:33:56 +0800

    tests: Add helper function to test if two havectos have same x, y, and z

--- src/tests/helpers.js
+++ src/tests/helpers.js
@@ -1,9 +1,13 @@
 const expectToBeEqualPoints = (point1, point2) => {
     expect(point1.id).toBe(point2.id)
     expect(point1.name).toBe(point2.name)
+    expectVectorsToHaveSameXYZ(point1, point2)
+}
+
+const expectVectorsToHaveSameXYZ = (point1, point2) => {
     expect(point1.x).toBeCloseTo(point2.x)
     expect(point1.y).toBeCloseTo(point2.y)
     expect(point1.z).toBeCloseTo(point2.z)
 }
 
-export { expectToBeEqualPoints }
+export { expectToBeEqualPoints, expectVectorsToHaveSameXYZ }
