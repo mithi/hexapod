1d8d12ca297abc2625e78b03f4726a1e0d4fdc4e
Mithi <mithi.sevilla@gmail.com>
2020-06-07 22:24:16 +0800

    minor-feat: Add landing page text v2

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,54 +1,52 @@
 import React from "react"
-import { BasicLink } from "../generic"
+import ReactMarkdown from "react-markdown"
+import { NavDetailed } from ".."
+import { PageLink } from "../generic"
+
+const landingPageMessage = `
+
+# Mithi's Bare Minimum Hexapod Robot Simulator
+
+Solve (and visualize) [forward][1] and [inverse][2] kinematics purely on your browser!
+Built with [React][3], [Plotly][7], and [MathJS][4].
+This is a complete rewrite of the [original][6] one I wrote in Python 🐍. Since
+this has zero server-side computations (which means no round trip) it should be extremely fast!
+
+I've been thinking of adding gait simulators and building more visualizers
+for other types of robot configurations (arms, drones, quadrupeds, and cars)
+and other robotics algorithms (model predictive control, kalman filters, path planning etc).
+Consider buying me a [couple cups of coffee 🍵 🍵 🍵][5] to help me get
+motivated!
+
+If you have ideas on how to make the source code quality better, I'd love to hear it! 💙
+I only learned Javascript, css, and React to make this project,
+so I'm not yet that familiar with best practices. Algorithm optimizations, test additions,
+and bug fixes (big or small) would also be extremely appreciated. [Submit an issue or pull request][6].
+
+## Love, Mithi 💙
+
+[1]: /forward-kinematics
+[2]: /inverse-kinematics
+[3]: https://reactjs.org/
+[4]: https://mathjs.org/
+[5]: https://ko-fi.com/minimithi
+[6]: https://github.com/mithi/hexapod-robot-simulator
+[7]: https://plot.ly/
+`
 
 class LandingPage extends React.Component {
     pageName = "Root"
-    // https://mithi.github.io/robotics-blog/v2-hexapod-1.gif
     componentDidMount() {
         this.props.onMount(this.pageName)
     }
+
     render = () => (
         <>
-            <div className="hexapod-img" />
-            <h2>Mithi's bare-minimum hexapod robot simulator.</h2>
-            <p>
-                It doesn't use any fancy libraries, only{" "}
-                <BasicLink path="math.js" klass="text" symbol="Math.js" />
-                for computations. Its only other dependencies are
-                <BasicLink path="plotly.js" klass="text" symbol="Plotly.js" />
-                for the 3d graph and
-                <BasicLink path="react.js" klass="text" symbol="React.js" />
-                for the user interface.
-            </p>
-            <p>
-                With this app you can simulate common
-                <BasicLink path="react.js" klass="text" symbol="gaits" />
-                and solve for the{" "}
-                <BasicLink path="react.js" klass="text" symbol="inverse" />
-                and <BasicLink path="react.js" klass="text" symbol="forward" />{" "}
-                kinematics. Here is a{" "}
-                <BasicLink path="react.js" klass="text" symbol="demonstation view" />
-                of this app in action.
-            </p>
-            <p>
-                I'm also working on bare-minimum visualizations for stuff like Quadrupeds,
-                Manipulators, Drones, Cars. If you like this, consider
-                <BasicLink
-                    path="react.js"
-                    klass="text"
-                    symbol="buying me a couple of cups of coffee 🍵"
-                />
-                to motivate me to make more cool stuff! You can also{" "}
-                <BasicLink
-                    path="react.js"
-                    klass="text"
-                    symbol="contribute to the source code"
-                />{" "}
-                or submit bug reports if you like.
-            </p>
-            <p>
-                Love, <BasicLink path="react.js" klass="text" symbol="💖💕 M i t h i" />
-            </p>
+            <PageLink path="./inverse-kinematics/">
+                <div className="hexapod-img" />{" "}
+            </PageLink>
+            <ReactMarkdown source={landingPageMessage} />
+            <NavDetailed />
         </>
     )
 }
