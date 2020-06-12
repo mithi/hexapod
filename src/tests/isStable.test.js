eed8af4f913bb49e04ce8b599be3cf5aefb808ce
Mithi <mithi.sevilla@gmail.com>
2020-06-12 21:03:09 +0800

    test: Add test for isStable function

---
+++ src/tests/isStable.test.js
@@ -0,0 +1,13 @@
+import { isStable } from "../hexapod/solvers/orientSolverHelpers"
+
+const cases = [
+    {
+        p0: { x: 300.0, y: 0.0, z: -100.0 },
+        p1: { x: 241.42, y: 241.42, z: -100 },
+        p2: { x: -300.0, y: 0, z: -100.0 },
+        answer: true,
+    },
+]
+test.each(cases)("test isStable:", example =>
+    expect(isStable(example.p0, example.p1, example.p2)).toBe(example.answer)
+)
