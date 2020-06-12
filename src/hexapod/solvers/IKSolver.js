b3fa60dcaed4658dc360b5e4f156a132ac4f41e5
Mithi <mithi.sevilla@gmail.com>
2020-06-12 21:02:52 +0800

    docs: Add function docstrings to IK Solver

--- src/hexapod/solvers/IKSolver.js
+++ src/hexapod/solvers/IKSolver.js
@@ -17,7 +17,76 @@ import {
     isCounterClockwise,
 } from "../geometry"
 
+/* * *
 
+.......
+Given:
+.......
+
+{}. Dimensions of each leg { femur, tibia, gamma }
+[] bodyContactPoints
+[] groundContactPoints
+   - there are two lists which contains six items each. One item for each leg.
+{} axes { xAxis, zAxis }
+   xAxis and zAxis of the rotated hexapod's local coordinate frame wrt
+   world coordinate frame.
+
+.......
+Definition:
+.......
+
+bodyContactPoint (x, y, z)
+- The point in 3d space which is a vertex of the hexagon.
+  This is where the leg is in contact with the body of the hexapod.
+
+groundContactPoint (x, y, z)
+- The point in 3d space which we want the foot tip of
+  the leg to be. Where the leg is in contact with the ground plane of the world.
+
+.......
+Find:
+.......
+
+18 angles that represent the pose of the hexapod. Three angles for each leg.
+    {
+      rightMiddle: { alpha, beta, gamma },
+      ...
+      rightBack: { alpha, beta, gamma },
+    }
+
+    If no solution is found, make sure to explain why.
+
+.......
+Algorithm:
+.......
+
+If one of the vertices is below the ground z < 0,
+then there is no solution. Early exit.
+
+For each leg:
+    1. Derive a few properties about the leg given what you already know
+       which you'd later (see computeInitialProperties() for details )
+
+       This includes the coxiaPoint. If this coxiaPoint is below the ground
+        - then there is no solution. Early exit.
+
+    2. Compute the alpha of this leg. see (computeAlpha())
+       If alpha is not within range, then there is no solution. Early exit.
+
+    3. Solve for beta and gamma of this leg (see LegIKSolver module)
+      If a problem was encountered within this module, then there is no solution. Early exit.
+      If the beta and gamma are not within range, then there is no solution, early exit.
+
+    4. Sometimes the LegIKSolver module would return a solution where the leg
+       would not reach the target ground contact point. (this leg would be on the air)
+       If the combination of the legs in the air would produce an unstable pose
+       (e.g 4 legs are in the air or all left legs are in the air)
+       Then there is no solution. Early exit.
+       (see also HexapodSupportChecker)
+
+    If no problems are encountered, we have found a solution! Return!
+
+* * */
 class IKSolver {
     params = {}
     partialPose = {}
@@ -131,19 +200,50 @@ class IKSolver {
     }
 }
 
-const computeAlpha = (coxiaVector, legXaxisAngle, xAxis, zAxis) => {
-    const sign = isCounterClockwise(coxiaVector, xAxis, zAxis) ? -1 : 1
-    const alphaWrtHexapod = sign * angleBetween(coxiaVector, xAxis)
-    const alpha = (alphaWrtHexapod - legXaxisAngle) % 360
-    if (alpha > 180) {
-        return alpha - 360
-    }
-    if (alpha < -180) {
-        return alpha + 360
-    }
-    return alpha
-}
+/* * *
 
+computeInitialLegProperties()
+
+.......
+Given:
+.......
+
+1. pB : bodyContactPoint in 3d space
+2. pG : groundContactPoint in 3d space
+3. coxia: distance from pB to pC
+4. zAxis: The vector normal to the hexapodBodyPlane
+
+.......
+Find:
+.......
+
+1. pC : coxiaPoint in 3d space
+2. coxiaVector: the vector from pB to Pc with a length of one
+3. coxiaUnitVector: A vector with the length of one
+    pointing at the direction of the unit vector
+4. rho: The angle made by pC, pB and pG, with pB at the center
+5. summa: The distance from pB to pG
+
+pB   pC
+ *---* -------- hexapodBodyPlane
+  \   \
+   \   *
+    \  /
+      * ------- groundPlane
+      pG
+
+.......
+Idea:
+.......
+
+1. Get the vector from pB to pG (bodyToFootVector)
+2. Project that vector to the hexapodBodyPlane (coxiaDirectionVector)
+   The direction of this vector is the direction of
+   coxiaVector and coxiaUnitVector
+
+   And with a little bit of geometry you derive verything you need.
+
+ * * */
 const computeInitialLegProperties = (
     bodyContactPoint,
     groundContactPoint,
@@ -151,10 +251,13 @@ const computeInitialLegProperties = (
     coxia
 ) => {
     const bodyToFootVector = vectorFromTo(bodyContactPoint, groundContactPoint)
+
     const coxiaDirectionVector = projectedVectorOntoPlane(bodyToFootVector, zAxis)
     const coxiaUnitVector = getUnitVector(coxiaDirectionVector)
     const coxiaVector = scaleVector(coxiaUnitVector, coxia)
+
     const coxiaPoint = addVectors(bodyContactPoint, coxiaVector)
+
     const rho = angleBetween(coxiaUnitVector, bodyToFootVector)
     const summa = vectorLength(bodyToFootVector)
 
@@ -165,6 +268,65 @@ const computeInitialLegProperties = (
         rho,
         summa,
     }
+}
+
+/* * *
+
+computeAlpha()
+
+  hexapodYaxis
+  ^
+  |
+  * --> hexapodXaxis (xAxis)
+ /
+hexapodZaxis (zAxis)
+
+...............
+Example #1 :
+...............
+
+             coxiaVector
+              ^
+              | legXaxis
+              |  /        * legXaxisAngle
+              | /            - Angle between legXaxis and hexapodXaxis
+    * -- * -- *                (in this example: +45 degrees )
+   /           \
+  /             \          * Alpha
+ *       *       *            - Angle between legXaxis and coxiaVector
+  \             /                (in this example: +45 degrees)
+   \           /
+    * -- * -- *
+
+...............
+Example #2
+...............
+
+    * -- * -- *       * legXaxisAngle
+   /           \          - (in this example: -45 degrees or +315 degrees)
+  /             \      * Alpha
+ *       *       *         - (in this example: -45 degrees)
+  \             /
+   \           /
+    * -- * -- *
+              |\              - (in this example: +45 degrees )
+              | \
+              | legXaxis
+              V
+              coxiaVector
+ * * */
+const computeAlpha = (coxiaVector, legXaxisAngle, xAxis, zAxis) => {
+    const sign = isCounterClockwise(coxiaVector, xAxis, zAxis) ? -1 : 1
+    const alphaWrtHexapod = sign * angleBetween(coxiaVector, xAxis)
+    const alpha = (alphaWrtHexapod - legXaxisAngle) % 360
+
+    if (alpha > 180) {
+        return alpha - 360
+    }
+    if (alpha < -180) {
+        return alpha + 360
+    }
+    return alpha
 }
 
 export default IKSolver
