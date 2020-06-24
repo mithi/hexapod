c57c1822b5dfb1f2b79bdbde754bbbc1da27dabb
Mithi <mithi.sevilla@gmail.com>
2020-06-24 10:25:09 +0800

    test: Add App test (error)

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,9 +1,24 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen} from "@testing-library/react"
 import App from "../../App"
 
 describe("App", () => {
     test("renders App component", () => {
         render(<App />)
+        screen.debug()
     })
 })
+
+/*
+
+Nav
+NavDetailed
+Sliders
+Posetable
+DimensionsWidget
+InverseKinematicsPage
+ForwardKinematicsPage
+LagPatternsPage
+LandingPage
+AlertBox
+*/
