46ae3b4a126e8a241126722692c8bd06e2b4dee4
Mithi <mithi.sevilla@gmail.com>
2020-06-26 01:48:02 +0800

    test: Add another IK test

--- src/tests/ikSolver.test.js
+++ src/tests/ikSolver.test.js
@@ -1,10 +1,11 @@
 import solveInverseKinematics from "../hexapod/solvers/ik/hexapodSolver"
 import CASE1 from "./cases/ikSolver/case1"
 import CASE2 from "./cases/ikSolver/case2"
+import CASE3 from "./cases/ikSolver/case3"
 
 import { expectToBeEqualPose } from "./helpers"
 
-const CASES = [CASE1, CASE2]
+const CASES = [CASE1, CASE2, CASE3]
 
 test.each(CASES)("IK Solver %p", thisCase => {
     const { dimensions, ikParams } = thisCase.params
