aed88f339c9c3300b5fde553329c4076becc885f
Mithi Sevilla <mithi.sevilla@gmail.com>
2020-07-07 01:26:02 +0800

    refactor: Move some unused app state down to its children (#104)
    chore: Build shouldn't fail when coverage is reduced

--- src/components/pages/InverseKinematicsPage.js
+++ src/components/pages/InverseKinematicsPage.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react"
 import { sliderList, Card, BasicButton } from "../generic"
 import { solveInverseKinematics } from "../../hexapod"
-import { DEFAULT_IK_PARAMS } from "../../templates"
 import { SECTION_NAMES, IK_SLIDERS_LABELS, RESET_LABEL } from "../vars"
+import { DEFAULT_IK_PARAMS } from "../../templates"
 
-const _updatedStateParamsUnsolved = (ikParams, message) => ({
-    ikParams,
+const _updatedStateParamsUnsolved = message => ({
     showPoseMessage: false,
     showInfo: true,
     info: { ...message, isAlert: true },
 })
 
-const _updatedStateParamsSolved = (ikParams, message) => ({
-    ikParams,
+const _updatedStateParamsSolved = message => ({
     showPoseMessage: true,
     showInfo: false,
     info: { ...message, isAlert: false },
@@ -20,37 +18,42 @@ const _updatedStateParamsSolved = (ikParams, message) => ({
 
 class InverseKinematicsPage extends Component {
     pageName = SECTION_NAMES.inverseKinematics
+    state = { ikParams: DEFAULT_IK_PARAMS }
 
     componentDidMount() {
         this.props.onMount(this.pageName)
     }
 
     updateIkParams = (name, value) => {
-        const ikParams = { ...this.props.params.ikParams, [name]: value }
+        const ikParams = { ...this.state.ikParams, [name]: value }
+        this.setState({ ikParams })
+
         const result = solveInverseKinematics(this.props.params.dimensions, ikParams)
 
         if (!result.obtainedSolution) {
-            const stateParams = _updatedStateParamsUnsolved(ikParams, result.message)
+            const stateParams = _updatedStateParamsUnsolved(result.message)
             this.props.onUpdate(null, stateParams)
             return
         }
 
-        const stateParams = _updatedStateParamsSolved(ikParams, result.message)
+        const stateParams = _updatedStateParamsSolved(result.message)
         this.props.onUpdate(result.hexapod, stateParams)
     }
 
     reset = () => {
-        const dimensions = this.props.params.dimensions
-        const result = solveInverseKinematics(dimensions, DEFAULT_IK_PARAMS)
-
-        const stateParams = _updatedStateParamsSolved(DEFAULT_IK_PARAMS, result.message)
+        this.setState({ DEFAULT_IK_PARAMS })
+        const result = solveInverseKinematics(
+            this.props.params.dimensions,
+            DEFAULT_IK_PARAMS
+        )
+        const stateParams = _updatedStateParamsSolved(result.message)
         this.props.onUpdate(result.hexapod, stateParams)
     }
 
     get sliders() {
         return sliderList({
             names: IK_SLIDERS_LABELS,
-            values: this.props.params.ikParams,
+            values: this.state.ikParams,
             handleChange: this.updateIkParams,
         })
     }
