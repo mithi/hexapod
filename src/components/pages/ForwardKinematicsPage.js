3f8f6f3ba17fed43bed102d41fa61dda091ce90c
Mithi <mithi.sevilla@gmail.com>
2020-07-15 02:42:57 +0800

    FIX: Pass simple string instead of converting component to string

--- src/components/pages/ForwardKinematicsPage.js
+++ src/components/pages/ForwardKinematicsPage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import { renderToString } from "react-dom/server"
 import LegPoseWidget from "./LegPoseWidgets"
 import { Card, ToggleSwitch, ResetButton, NumberInputField, Slider } from "../generic"
 import { DEFAULT_POSE } from "../../templates"
@@ -41,7 +40,7 @@ class ForwardKinematicsPage extends Component {
     get toggleSwitch() {
         const props = {
             id: "FwdKinematicsSwitch",
-            value: renderToString(this.state.WidgetType),
+            value: this.state.WidgetType === Slider ? "Slider" : "NumberInputField",
             handleChange: this.toggleMode,
             showValue: false,
         }
