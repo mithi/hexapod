9ec687d8bb0918fdc439ed2bdd642e875c790d57
Mithi <mithi.sevilla@gmail.com>
2020-07-21 04:01:23 +0800

    optimize: Remove redundant nav

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { NavDetailed } from ".."
 import { SECTION_NAMES } from "../vars"
 
 class LandingPage extends React.Component {
@@ -13,7 +12,6 @@ class LandingPage extends React.Component {
                 <h1>Mithi's Hexapod Robot Simulator</h1>
                 <p>Enjoy your stay and share with your friends!</p>
             </div>
-            <NavDetailed />
         </>
     )
 }
