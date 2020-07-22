c9421de764bd428f705a8d3a31d13c251210e322
Mithi <mithi.sevilla@gmail.com>
2020-07-22 11:40:29 +0800

    optimize: Move css to bottom of page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
-import "./font.css"
+
+//import "./index.css"
+//import "./font.css"
 import * as serviceWorker from "./serviceWorker"
 
 const App = React.lazy(() => import("./App"))
