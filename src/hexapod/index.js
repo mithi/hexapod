467d1a3b92dabd0304c7ef4675d64179f82efb69
2Shar18 <tushar.rajdev@gmail.com>
2020-08-09 18:28:13 +0530

    Move: hexapod/plotter to templates/plotter
    Move: hexapod/plotter to templates/plotter
    
    Move: hexapod/plotter to templates/plotter
    
    HexapodPlot fix

--- src/hexapod/index.js
+++ src/hexapod/index.js
@@ -1,5 +1,5 @@
 import VirtualHexapod from "./VirtualHexapod"
 import solveInverseKinematics from "./solvers/ik/hexapodSolver"
-import getNewPlotParams from "./plotter"
+import getNewPlotParams from "../templates/plotter"
 import { POSITION_NAMES_LIST } from "./constants"
 export { VirtualHexapod, getNewPlotParams, solveInverseKinematics, POSITION_NAMES_LIST }
